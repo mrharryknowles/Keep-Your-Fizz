9ebb6f98c623fcf372c628514a8da90063fce575
mrharryknowles <148089807+mrharryknowles@users.noreply.github.com>
2025-01-26 11:08:31 +0000

    Enemy sprite reflects damage taken

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,40 @@ public class Enemy : MonoBehaviour
 {
     public float health = 30f;
 
+    public Sprite fullHealthSprite;
+    public Sprite midHealthSprite;
+    public Sprite lowHealthSprite;
+
+    public SpriteRenderer spriteRenderer;
+
     //method to take damage from the player/can
     public virtual void TakeDamage(float damage)
     {
         health -= damage;
+
+        UpdateSprite();
 
         //if their health is less than or equal to 0, destroy the enemy
         if (health <= 0)
         {
             Destroy(gameObject); // destroy the enemy
         }
+    }
+
+    private void UpdateSprite()
+    {
+        if (health > 20)
+        {
+            spriteRenderer.sprite = fullHealthSprite;
+        }
+        else if (health > 10)
+        {
+            spriteRenderer.sprite = midHealthSprite;
+        }
+        else
+        {
+            spriteRenderer.sprite = lowHealthSprite;
+        }
+        
     }
 }
