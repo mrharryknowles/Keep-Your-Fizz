50fc94582a4d245251e925751c03c6fb831d979b
mrharryknowles <148089807+mrharryknowles@users.noreply.github.com>
2025-01-26 10:34:09 +0000

    updated enemy script - spawn intervals altered and calculated via time elapsed

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,20 +6,27 @@ public class EnemySpawner : MonoBehaviour
 {
 
     public GameObject enemyPrefab; //reference to the enemy prefab
-    public float spawnInterval = 3f; //time between the spawns
+    public float initialSpawnInterval = 3f; //time between the spawns
     public float spawnRadius = 10f; //distance from the centre to spawn the enemies
     public Transform playerTransform; //reference to the player
+    public float intervalScale = 10f; //scaling factor for the spawn-speedup
+
+    private float elapsedTime = 0; //tracks the total elapsed time/game duration
 
     private float counter = 0f; //timer to track the spawn intervals
 
     void FixedUpdate()
     {
+        elapsedTime += Time.fixedDeltaTime; //update elapsed time, increment every frame
         counter += Time.fixedDeltaTime; //increment the timer
+
+        float spawnInterval = initialSpawnInterval / Mathf.Max(Mathf.Sqrt(elapsedTime / intervalScale), 1);
+        //spawn calculated based on the elapsed time
 
         if (counter > spawnInterval)
         {
             counter = 0f;//reset the timer
-            SpawnEnemy();
+            SpawnEnemy(); //spawn an enemy
         }
 
     }
